380d607da2ba7ec1d1f59b8e2cc194ae285ba166
Tsiaro Fitia <fitia.rajs.12@gmail.com>
2025-03-06 20:34:09 +0300

    ajout de calendar sur ADDTRANSACTION (selecteur de calendrier)

--- src/components/Add/ModalAddTransaction.js
+++ src/components/Add/ModalAddTransaction.js
@@ -1,22 +1,37 @@
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import React, { Component } from 'react';
-import { Ionicons } from '@expo/vector-icons';
-import { Picker } from '@react-native-picker/picker';
-import Colors from '../../outils/Colors';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import React, { Component } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
+import { Calendar } from "react-native-calendars";
+import Colors from "../../outils/Colors";
 
 export default class ModalAddTransaction extends Component {
   state = {
-    amount: '',
-    category: '',
-    date: '',
-    description: '',
-    transactionType: 'revenue', // Par défaut, c'est un revenu
+    amount: "",
+    category: "",
+    date: "",
+    description: "",
+    transactionType: "revenue", // Par défaut, c'est un revenu
+    showCalendar: false,
   };
 
   handleAddTransaction = () => {
     const { description, amount, transactionType, category, date } = this.state;
     if (description && amount && category && date) {
-      this.props.onAddTransaction({ description, amount, transactionType, category, date });
+      this.props.onAddTransaction({
+        description,
+        amount,
+        transactionType,
+        category,
+        date,
+      });
     }
   };
 
@@ -24,59 +39,161 @@ export default class ModalAddTransaction extends Component {
     this.setState({ transactionType: type });
   };
 
+  handleDateSelect = (day) => {
+    const selectedDate = `${day.year}-${day.month}-${day.day}`;
+    this.setState({ date: selectedDate, showCalendar: false });
+  };
+
   render() {
-    const { transactionType } = this.state;
-    const iconColor = transactionType === 'Income' ? Colors.Vert : Colors.Rouge;
-    const buttonStyle = transactionType === 'Income' ? styles.selectedButtonGreen : styles.selectedButtonRed;
+    const { transactionType, date, showCalendar } = this.state;
+    const iconColor = transactionType === "Income" ? Colors.Vert : Colors.Rouge;
+    const buttonStyle =
+      transactionType === "Income"
+        ? styles.selectedButtonGreen
+        : styles.selectedButtonRed;
 
     return (
       <View style={styles.modalContainer}>
         <View style={styles.headertitle}>
-            <Text style={styles.title}>Add a new Transaction</Text>
+          <Text style={styles.title}>Add a new Transaction</Text>
         </View>
         <View style={styles.tsipika}></View>
-      
+
         {/* Sélectionner le type de transaction (Revenu ou Dépense) */}
         <View style={styles.transactionTypeContainer}>
           <TouchableOpacity
-            style={[styles.transactionButton, transactionType === 'Income' && buttonStyle]}
-            onPress={() => this.handleTransactionTypeChange('Income')}
+            style={[
+              styles.transactionButton,
+              transactionType === "Income" && buttonStyle,
+            ]}
+            onPress={() => this.handleTransactionTypeChange("Income")}
           >
-            <Ionicons name="arrow-up-circle-outline" size={24} color={transactionType === 'Income' ? Colors.Vert : Colors.GrisNeutre} style={{ paddingLeft: 10,}}/>
-            <Text style={[styles.transactionButtonText, { color: Colors.Vert }]}>Income</Text>
+            <Ionicons
+              name="arrow-up-circle-outline"
+              size={24}
+              color={
+                transactionType === "Income" ? Colors.Vert : Colors.GrisNeutre
+              }
+              style={{ paddingLeft: 10 }}
+            />
+            <Text
+              style={[styles.transactionButtonText, { color: Colors.Vert }]}
+            >
+              Income
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            style={[styles.transactionButton, transactionType === 'expense' && buttonStyle]}
-            onPress={() => this.handleTransactionTypeChange('expense')}
+            style={[
+              styles.transactionButton,
+              transactionType === "expense" && buttonStyle,
+            ]}
+            onPress={() => this.handleTransactionTypeChange("expense")}
           >
-            <Ionicons name="arrow-down-circle-outline" size={24} color={transactionType === 'expense' ? Colors.Rouge : Colors.GrisNeutre} style={{ paddingLeft: 10,}} />
-            <Text style={[styles.transactionButtonText, { color: Colors.Rouge }]}>Expense</Text>
+            <Ionicons
+              name="arrow-down-circle-outline"
+              size={24}
+              color={
+                transactionType === "expense" ? Colors.Rouge : Colors.GrisNeutre
+              }
+              style={{ paddingLeft: 10 }}
+            />
+            <Text
+              style={[styles.transactionButtonText, { color: Colors.Rouge }]}
+            >
+              Expense
+            </Text>
           </TouchableOpacity>
         </View>
 
         {/* Champs (Montant, Catégorie, Date, Description) avec icône en couleur */}
-        {['Amount', 'Category', 'Date', 'Description'].map((field, index) => (
+        {["Amount", "Category", "Description"].map((field, index) => (
           <TouchableOpacity
             key={index}
             style={styles.inputContainer}
             onPress={() => this[`inputRef${field}`]?.focus()}
           >
-            <Ionicons 
-              name={field === 'Amount' ? "cash-outline" : field === 'Category' ? "folder-outline" : field === 'Date' ? "calendar-outline" : "pencil-outline"} 
-              size={24} 
-              color={iconColor} 
-              style={{marginLeft:10,}}
+            <Ionicons
+              name={
+                field === "Amount"
+                  ? "cash-outline"
+                  : field === "Category"
+                  ? "folder-outline"
+                  : "pencil-outline"
+              }
+              size={24}
+              color={iconColor}
+              style={{ marginLeft: 10 }}
             />
             <TextInput
               ref={(input) => (this[`inputRef${field}`] = input)}
               style={styles.input}
               placeholder={field}
               value={this.state[field.toLowerCase()]}
-              onChangeText={(text) => this.setState({ [field.toLowerCase()]: text })}
+              onChangeText={(text) =>
+                this.setState({ [field.toLowerCase()]: text })
+              }
             />
           </TouchableOpacity>
         ))}
+
+        {/* Sélecteur de date */}
+        <TouchableOpacity
+          style={styles.inputContainerDate}
+          onPress={() => this.setState({ showCalendar: true })}
+        >
+          <Ionicons
+            name="calendar-outline"
+            size={24}
+            color={iconColor}
+            style={{ marginLeft: 10 }}
+          />
+          <Text style={styles.input}>
+            {date
+              ? new Date(date).toLocaleDateString("fr-FR", {
+                  weekday: "short",
+                  day: "numeric",
+                  month: "short",
+                })
+              : "Date"}
+          </Text>
+        </TouchableOpacity>
+
+        {/* Calendrier */}
+        <Modal
+          transparent={true}
+          visible={showCalendar}
+          onRequestClose={() => this.setState({ showCalendar: false })}
+        >
+          <View style={styles.calendarModal}>
+            <View style={styles.calendarContainer}>
+              <Calendar
+                onDayPress={(day) => this.handleDateSelect(day)}
+                markedDates={{
+                  [date]: { selected: true, selectedColor: Colors.BleuFoncé },
+                }}
+                theme={{
+                  backgroundColor: Colors.white,
+                  calendarBackground: Colors.white,
+                  textSectionTitleColor: Colors.BleuFoncé,
+                  selectedDayBackgroundColor: Colors.BleuClair,
+                  selectedDayTextColor: Colors.white,
+                  todayTextColor: Colors.BleuFoncé,
+                  dayTextColor: Colors.BleuFoncé,
+                  textDisabledColor: Colors.BleuClair,
+                  arrowColor: Colors.BleuClair,
+                  monthTextColor: Colors.BleuFoncé,
+                  textDayFontWeight: "medium",
+                  textMonthFontWeight: "bold",
+                  textDayHeaderFontWeight: "bold",
+                  textDayFontSize: 14,
+                  textMonthFontSize: 20,
+                  textDayHeaderFontSize: 16,
+                }}
+              />
+            </View>
+          </View>
+        </Modal>
 
         {/* Boutons */}
         <View style={styles.buttonContainer}>
@@ -84,16 +201,33 @@ export default class ModalAddTransaction extends Component {
             style={[styles.button, styles.cancelButton]}
             onPress={this.props.onClose}
           >
-            
-            <Text style={[styles.buttonTextCancel, { color: Colors.JauneFoncé }]}>Cancel</Text>
+            <Text
+              style={[styles.buttonTextCancel, { color: Colors.JauneFoncé }]}
+            >
+              Cancel
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            style={[styles.button, transactionType === 'Income' ? styles.buttonGreen : styles.buttonRed]}
+            style={[
+              styles.button,
+              transactionType === "Income"
+                ? styles.buttonGreen
+                : styles.buttonRed,
+            ]}
             onPress={this.handleAddTransaction}
           >
-            
-            <Text style={[styles.buttonText, { color: transactionType === 'Income' ? Colors.Vert : Colors.Rouge }]}>Valid</Text>
+            <Text
+              style={[
+                styles.buttonText,
+                {
+                  color:
+                    transactionType === "Income" ? Colors.Vert : Colors.Rouge,
+                },
+              ]}
+            >
+              Valid
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -102,34 +236,40 @@ export default class ModalAddTransaction extends Component {
 }
 
 const styles = StyleSheet.create({
-
   modalContainer: {
     backgroundColor: Colors.white,
-    width: '90%',
+    width: "90%",
     padding: 20,
     borderRadius: 10,
     elevation: 10,
-    justifyContent: 'center',
-    alignItems: 'stretch',
+    justifyContent: "center",
+    alignItems: "stretch",
   },
   title: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
     color: Colors.BleuFoncé,
   },
-  tsipika:{
-    height:1,
+  tsipika: {
+    height: 1,
     backgroundColor: Colors.Gris,
     marginBottom: 20,
-
   },
   inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: Colors.Gris,
     padding: 7,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  inputContainerDate: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.Gris,
+    padding: 15,
     borderRadius: 8,
     marginBottom: 20,
   },
@@ -139,49 +279,49 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   transactionTypeContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 20,
   },
   transactionButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: Colors.BlancFond,
     padding: 10,
     borderRadius: 10,
-    width: '48%',
-    height:50,
+    width: "48%",
+    height: 50,
     elevation: 3,
     marginBottom: 10,
     marginTop: 20,
   },
   selectedButtonGreen: {
-    borderWidth:1,
+    borderWidth: 1,
     borderColor: Colors.Vert,
   },
   selectedButtonRed: {
-    borderWidth:1,
+    borderWidth: 1,
     borderColor: Colors.Rouge,
   },
   transactionButtonText: {
     fontSize: 16,
-    color: '#000',
+    color: "#000",
     paddingLeft: 15,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 20,
-    marginBottom:15,
+    marginBottom: 15,
   },
   button: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: Colors.BleuFoncé,
     padding: 10,
     borderRadius: 8,
-    width: '45%',
-    justifyContent: 'center',
+    width: "45%",
+    justifyContent: "center",
   },
   buttonGreen: {
     backgroundColor: Colors.BlancFond,
@@ -205,5 +345,18 @@ const styles = StyleSheet.create({
   buttonTextCancel: {
     fontSize: 16,
     marginLeft: 5,
+  },
+  calendarModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  calendarContainer: {
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
+    width: "90%",
   },
 });
